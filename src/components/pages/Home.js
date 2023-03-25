5dd0176ec807acabbfd6362a0d82797897ee752c
Gustavo Garcia <gustavogarcia42@yahoo.com>
2023-03-24 19:35:21 -0500

    added new bio

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,19 +15,22 @@ export default function Home() {
 
       <div className="md:w-2/4 md:mx-auto bg-slate-100/60 rounded-xl px-8 py-4 md:px-8 tracking-tight text-center">
         <p className="text-sm font-mono md:text-base">
-          Aspiring web developer. Currently enrolled in the SMU Coding Bootcamp.
-          I love playing video games and watching sports, mostly soccer. Some of
-          my coding skills are Javascript, NodeJS, ReactJS, MongoDB, and Good
-          ol' HTML. I currently work in the food industry but I've decided to
-          take a different career path, and jump into the programming industry.
+          Certified Full Stack Web Developer from SMU Coding Bootcamp. My
+          programming language of choice is Javascript, but also know Python.
+          With Javascript I use NodeJS for my backend, MangoDb as my database,
+          Mongoose as my ORM, and React takes care of my front end. I currently
+          work in the food industry but I've decided to take a different career
+          path, and jump into the programming industry.
         </p>
         <button
           type="button"
           className="m-3 p-2 bg-cyan-600 hover:bg-cyan-300 hover:text-slate-700 rounded-lg text-slate-200"
         >
-          <a className="text-md font-medium" href={resume}>
-            Resume
-          </a>
+          <object>
+            <a className="text-md font-medium" href={resume}>
+              Resume
+            </a>
+          </object>
         </button>
       </div>
     </div>
